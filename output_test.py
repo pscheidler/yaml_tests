a0e1d80e67a54c69999b673bb6c6c9e8d7a265f5
Pete Scheidler <pscheidler@gmail.com>
2026-08-16 12:15:04 -0400

    Fixing the JSON output

--- output_test.py
+++ output_test.py
@@ -3,6 +3,6 @@ import json
 
 print("Script starting", file=sys.stderr)
 output = {"element1": "Foo", "element2": 2, "element3": "bar"}
-print("I'm a screw up!")
+# print("I'm a screw up!")
 print(json.dumps(output))
 print("Script ending", file=sys.stderr)
