990604487ed5c6639267eb664fc6aff252a38434
Pete Scheidler <pscheidler@gmail.com>
2026-08-28 10:00:41 -0400

    Testing JSON input to Python

--- output_test.py
+++ output_test.py
@@ -1,8 +1,23 @@
 import sys
 import json
+import os
 
-print("Script starting", file=sys.stderr)
+PRINT_TARGET = os.getenv("SCRIPT_PRINT_TARGET")
+
+def log(msg, print_target=PRINT_TARGET):
+    if not print_target:
+        print(msg)
+    elif print_target.lower() == "stderr":
+        print(msg, file=sys.stderr)
+    elif print_target.lower() == "none":
+        return
+    else:
+        raise Exception(f"Unknown print target: {print_target}")
+
+log("::group::Script Output")
+log("Script starting")
 output = {"element1": "Foo", "element2": 2, "element3": "bar"}
 # print("I'm a screw up!")
 print(json.dumps(output))
-print("Script ending", file=sys.stderr)
+log("Script ending")
+log("::endgroup::")
